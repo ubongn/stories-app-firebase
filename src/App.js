663ae0b54d29c2dbfdadb1e3e7d8782788d95885
ubong <ubongnt@gmail.com>
2022-03-03 06:51:34 +0100

    set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__top">
+        <img src="logo192.png" alt="App Logo" className="app__logo" />
+        <h1>Shorts</h1>
+      </div>
+      <div className="app__videos"></div>
     </div>
   );
 }
