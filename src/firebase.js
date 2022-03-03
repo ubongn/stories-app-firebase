98fd45f153ebd9aa6cf40e87bb2f32dde5ab2707
ubong <ubongnt@gmail.com>
2022-03-03 06:39:37 +0100

    clean up

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDUZDiI_EwK8FZctiZw3OSwhqO2J5tmJmM",
+    authDomain: "stories-app-74688.firebaseapp.com",
+    projectId: "stories-app-74688",
+    storageBucket: "stories-app-74688.appspot.com",
+    messagingSenderId: "1088582654090",
+    appId: "1:1088582654090:web:ed2ca9012b94d30576f573"
+  };
